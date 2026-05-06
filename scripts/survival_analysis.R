91a0f686d153e17b60a767023e59955ee403fb5c
YihengMao1 <1975616062@qq.com>
2026-05-07 08:15:01 +1000

    Add Kaplan-Meier survival curve plot

--- scripts/survival_analysis.R
+++ scripts/survival_analysis.R
@@ -18,3 +18,21 @@ cox_model <- coxph(Surv(time, status) ~ age + sex, data = lung)
 
 # Model summary
 summary(cox_model)
+
+# Install package if needed
+install.packages("survminer")
+
+# Load package
+library(survminer)
+
+# Plot Kaplan-Meier curve
+km_plot <- ggsurvplot(
+  km_fit,
+  data = lung,
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE
+)
+
+# Save plot
+ggsave("km_curve.png", plot = km_plot$plot)
